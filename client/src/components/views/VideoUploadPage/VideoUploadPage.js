705162fa8eff54a97b1117463a287d98486b6c7f
hj <20125182@hanmail.net>
2021-10-13 22:34:41 +0900

    upload form Template

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -1,7 +1,62 @@
 import React from 'react';
+import { Typography, Button, Form, message, Input } from 'antd';
+import Icon from '@ant-design/icons';
+
+const { Title } = Typography;
+const { Textarea } = Input;
 
 function VideoUploadPage() {
-  return <div></div>;
+  return (
+    <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
+      <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+      <Form onSubmit>
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          {/* drop Zone */}
+
+          {/* Thumbnail */}
+
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+
+        <br />
+        <br />
+
+        <label>Title</label>
+        <Input onChange="" value="" />
+
+        <br />
+        <br />
+
+        <label>Description</label>
+        <Textarea onChange="" value="" />
+
+        <br />
+        <br />
+
+        <select onChange="">
+          <option key="" value=""></option>
+        </select>
+
+        <br />
+        <br />
+
+        <select onChange="">
+          <option key="" value=""></option>
+        </select>
+
+        <br />
+        <br />
+
+        <Button type="primary" size="large" onClick="">
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
 export default VideoUploadPage;
