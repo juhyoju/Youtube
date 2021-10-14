3adcdcb5466da6a906b33f0454a538a8903f636d
hj <20125182@hanmail.net>
2021-10-14 20:04:52 +0900

    create onDrop function

--- client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Typography, Button, Form, message, Input } from 'antd'
 import { PlusOutlined } from '@ant-design/icons'
 import Dropzone from 'react-dropzone'
+import Axios from 'axios'
 
 const { Title } = Typography
 const { TextArea } = Input
@@ -40,6 +41,22 @@ function VideoUploadPage() {
     setCategory(e.currentTarget.value)
   }
 
+  const onDrop = files => {
+    let formData = new FormData()
+    const config = {
+      // server에 request를 받거나 보낼 때의 오류 방지 위함
+      header: { 'content-type': 'multipart/form-data' }
+    }
+    formData.append('file', files[0])
+
+    Axios.post('/api/video/uploadfiles', formData, config).then(response => {
+      if (response.data.success) {
+      } else {
+        alert('비디오 업로드 실패')
+      }
+    })
+  }
+
   return (
     <div style={{ maxWidth: '700px', margin: '2rem auto' }}>
       <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
@@ -49,7 +66,7 @@ function VideoUploadPage() {
         <div style={{ display: 'flex', justifyContent: 'space-between' }}>
           {/* drop Zone */}
 
-          <Dropzone onDrop='' multiple='' maxSize=''>
+          <Dropzone onDrop={onDrop} multiple={false} maxSize={1000000000}>
             {({ getRootProps, getInputProps }) => (
               <div
                 style={{
